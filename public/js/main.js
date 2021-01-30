6a3ad969a9d661c5e3c779033e29319a92acf8de
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:46:11 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+// exo 1
+
+// let nombre = 8.56
+// console.log(Math.round(nombre));
+
+// exo2
+
+// let nombre = 8.56
+// console.log(Math.ceil(nombre));
+
+// exo3
+
+// let nombre = 8.56
+// console.log(Math.floor(nombre));
+
+// exo4
+
+// let nombre = 8.56
+// console.log(Math.trunc(nombre));
+
+// exo5
+
+// let nombre = (Math.random());
+// console.log(nombre);
+
+// exo6
+
+// let nombre = (Math.random() *100);
+// console.log(nombre);
+
+// exo7
+
+// let nombre = Math.round((Math.random() *100));
+// console.log(nombre);
+
+// exo8
+
+// let nombre = Math.pow(8, 2);
+// console.log(nombre);
+
+// exo9
+
+// let nombre = Math.sqrt(9);
+// console.log(nombre);
+
+// exo10
+
+// let nombre = Math.abs(-1)
+// console.log(nombre);
+
+// exo11
+// let nombre = Math.max(-2, 1000, 8, 57);
+// console.log(nombre);
+
+// exo12
+
+// let nombre = Math.min(-2, 1000, 8, 57);
+// console.log(nombre);
+
